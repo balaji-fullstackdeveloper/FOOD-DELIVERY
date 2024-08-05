c9eb520234763b31b5aaa0e1bb39a948a9fcb9c8
BALAJI934428 <142514847+BALAJI934428@users.noreply.github.com>
2024-08-06 04:23:45 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,18 @@ dotenv.config({ path: path.join(__dirname, "utils/config.env") });
 const app = express();
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 connectDatabase();
 app.use("/api/food", foodRouter);
